c8655d7b946221fb7569cbede83e745c4af94158
aghifarii <alhamdanighifari@gmail.com>
2021-09-04 15:36:50 +0700

    ready to submit

--- MemeMe 2.0/MemeCollectionViewController.swift
+++ MemeMe 2.0/MemeCollectionViewController.swift
@@ -14,14 +14,13 @@ class MemeCollectionViewController: UICollectionViewController, UICollectionView
     @IBOutlet weak var memeFlowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var navigationBar: UINavigationItem!
     
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var memes: [Meme]! {
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
     }
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
+        
     override func viewWillAppear(_ animated: Bool) {
         self.collectionView!.reloadData()
         
@@ -29,7 +28,6 @@ class MemeCollectionViewController: UICollectionViewController, UICollectionView
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //Add new meme button
         let addMeme = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addMeme(_:)) )
         self.navigationBar.rightBarButtonItem = addMeme
@@ -52,23 +50,6 @@ class MemeCollectionViewController: UICollectionViewController, UICollectionView
             collectionView?.reloadData()
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
@@ -106,36 +87,4 @@ class MemeCollectionViewController: UICollectionViewController, UICollectionView
             collectionView!.backgroundView = nil
         }
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
